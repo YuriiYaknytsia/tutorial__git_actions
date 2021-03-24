014a803abc13d3987f9a8a26c4153abe3a05abe3
yuriy <yurii.yakhnytsia@baseblocks.tech>
2021-03-24 08:26:09 +0200

    Initial commit

---
+++ src/main/java/com/yurets_y/tutorial/git_actions/GitActionsApplication.java
@@ -0,0 +1,13 @@
+package com.yurets_y.tutorial.git_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurets_y/tutorial/git_actions/GitActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurets_y.tutorial.git_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class GitActionsApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
